1566c91700080b06bd76e189667763166701b599
Janyl <janyl.rk@gmail.com>
2022-04-11 16:42:35 +0600

    role is ready

--- src/main/java/kg/peaksoft/bilingualb4/models/Role.java
+++ src/main/java/kg/peaksoft/bilingualb4/models/Role.java
@@ -1,4 +1,19 @@
 package kg.peaksoft.bilingualb4.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@Table(name = "roles")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+
+
 }
