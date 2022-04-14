28cc6d00808dd6c5765c722716696a1c0d9aabaf
User <fgfdfgf>
2022-04-14 20:23:43 +0600

    add v2

--- src/main/resources/db/migration/V1__InitTables.sql
+++ src/main/resources/db/migration/V1__InitTables.sql
@@ -1,37 +1,32 @@
 
+create table roles
+(
+    id   bigserial primary key not null ,
+    name varchar(255)
+);
 create table auth_info(
 
-    id   bigserial primary key not null ,
-    email varchar not null unique ,
-    is_account_non_expired   boolean not null,
-    is_account_non_locked     boolean not null,
-    is_credentials_non_expired boolean not null,
-    is_enabled                 boolean not null,
-    password   varchar not null
+                          id   bigserial primary key not null ,
+                          email varchar not null unique ,
+                          is_account_non_expired   boolean not null,
+                          is_account_non_locked     boolean not null,
+                          is_credentials_non_expired boolean not null,
+                          is_enabled                 boolean not null,
+                          password   varchar not null
 );
 create table auth_info_roles
 (
-    auth_info_id bigserial primary key not null
-         references auth_info,
-    roles_id   bigserial primary key not null
-        references roles
-);
-create table roles
-(
-    id   bigint not null
-        constraint roles_pkey
-            primary key,
-    name varchar(255)
+    auth_info_id bigint unique not null
+        references auth_info(id),
+    roles_id   bigint unique not null
+        references roles(id)
 );
 create table users
 (
-    id           bigint not null
-        constraint users_pkey
-            primary key,
+    id  bigserial primary key not null ,
     email        varchar(255),
     password     varchar(255),
     user_name    varchar(255),
-    auth_info_id bigint
-        constraint fk2blmj1akvmy7gwdifggeajcy8
-            references auth_info
+    auth_id bigint unique
+        references auth_info(id)
 );
---
+++ src/main/resources/db/migration/V2__dataSql.sql
@@ -0,0 +1,11 @@
+insert into roles(id,name)
+values('amin'),
+('user');
+insert into auth_info( id  ,
+                       email ,
+                       is_account_non_expired,
+                       is_account_non_locked,
+                       is_credentials_non_expired,
+                       is_enabled,
+                       password)values
+
