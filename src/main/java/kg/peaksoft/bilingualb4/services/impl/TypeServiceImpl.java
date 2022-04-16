eb975aabf30726ac06bec36723c16fb0f2a6f289
User <sdfsdfsdfsd>
2022-04-16 12:58:49 +0600

    update is changed

--- src/main/java/kg/peaksoft/bilingualb4/services/impl/TypeServiceImpl.java
+++ src/main/java/kg/peaksoft/bilingualb4/services/impl/TypeServiceImpl.java
@@ -7,13 +7,10 @@ import kg.peaksoft.bilingualb4.exception.NotFoundException;
 import kg.peaksoft.bilingualb4.mappers.editMapper.TypeEditMapper;
 import kg.peaksoft.bilingualb4.mappers.viewMapper.TypeViewMapper;
 import kg.peaksoft.bilingualb4.models.Type;
-import kg.peaksoft.bilingualb4.models.Word;
 import kg.peaksoft.bilingualb4.models.enums.QuestionType;
-import kg.peaksoft.bilingualb4.models.enums.SingleAndMultiType;
 import kg.peaksoft.bilingualb4.repositories.TypeRepository;
 import kg.peaksoft.bilingualb4.services.TypeService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -88,97 +85,31 @@ public class TypeServiceImpl implements TypeService {
     public Type updateById(Long id, TypeRequest  typeRequest) {
         Type type = findById(id);
 
-        String currentName = type.getName();
-        String newName = typeRequest.getName();
+        boolean exists = typeRepository.existsById(id);
 
-        if (!currentName.equals(newName)){
-            type.setName(newName);
-        }
-        Object currentSingleAndMultiType = type.getSingleAndMultiType();
-        SingleAndMultiType newSingleAndMultiType = typeRequest.getSingleAndMultiType();
+        if (exists){
+            throw new BadRequestException(
+                    String.format("question with %d is already exists",id)
+            );
+        }else {
+            type.setName(typeRequest.getName());
+            type.setSingleAndMultiType(typeRequest.getSingleAndMultiType());
+            type.setWordList(typeRequest.getWordList());
+            type.setAudio(typeRequest.getAudio());
+            type.setNumberOfReplays(typeRequest.getNumberOfReplays());
+            type.setUpload(typeRequest.getUpload());
+            type.setPlay(typeRequest.getPlay());
+            type.setCorrectAnswer(typeRequest.isCorrectAnswer());
+            type.setRecord(typeRequest.getRecord());
+            type.setUploadImage(typeRequest.getUploadImage());
+            type.setQuestionStatement(typeRequest.getQuestionStatement());
+            type.setWordCounter(typeRequest.getWordCounter());
+            type.setQuestionToThePassage(typeRequest.getQuestionToThePassage());
+            type.setPassage(typeRequest.getPassage());
+            type.setHighlightCorrectAnswer(typeRequest.getHighlightCorrectAnswer());
 
-        if (!currentSingleAndMultiType.equals(newSingleAndMultiType)){
-            type.setSingleAndMultiType(newSingleAndMultiType);
         }
-        List<Word> currentListWord = type.getWordList();
-        List<Word> newListWord = typeRequest.getWordList();
-
-        if (!currentListWord.equals(newListWord)){
-            type.setWordList(newListWord);
-        }
-        String currentAudio = type.getAudio();
-        String  newAudio = typeRequest.getAudio();
-
-        if (!currentAudio.equals(newAudio)){
-            type.setAudio(newAudio);
-        }
-        Object currentNumberOfReplays = type.getNumberOfReplays();
-        int newNumberOfReplays = typeRequest.getNumberOfReplays();
-
-        if (!currentNumberOfReplays.equals(newNumberOfReplays)){
-            type.setNumberOfReplays(newNumberOfReplays);
-        }
-        String currentUpload = type.getUpload();
-        String newUpload = typeRequest.getUpload();
-
-        if (!currentUpload.equals(newUpload)){
-            type.setUpload(newUpload);
-        }
-        String currentPlay = type.getPlay();
-        String newPlay = typeRequest.getPlay();
-
-        if (!currentPlay.equals(newPlay)){
-            type.setPlay(newPlay);
-        }
-        Object currentCorrectAnswer  = type.isCorrectAnswer();
-        boolean newCorrectAnswer = typeRequest.isCorrectAnswer();
-
-        if (!currentCorrectAnswer.equals(newCorrectAnswer)){
-            type.setCorrectAnswer(newCorrectAnswer);
-        }
-        String currentRecord = type.getRecord();
-        String newRecord = typeRequest.getRecord();
-
-        if (!currentRecord.equals(newRecord)){
-            type.setRecord(newRecord);
-        }
-        String currentUploadImage = type.getUploadImage();
-        String newUploadImage = typeRequest.getUploadImage();
-
-        if (!currentUploadImage.equals(newUploadImage)){
-            type.setUploadImage(newUploadImage);
-        }
-        String currentQuestionStatement = type.getQuestionStatement();
-        String newQuestionStatement = typeRequest.getQuestionStatement();
-
-        if (!currentQuestionStatement.equals(newQuestionStatement)){
-            type.setQuestionStatement(newQuestionStatement);
-        }
-        Object currentWordCounter = type.getWordCounter();
-        int newWordCounter = typeRequest.getWordCounter();
-
-        if (!currentWordCounter.equals(newWordCounter)){
-            type.setWordCounter(newWordCounter);
-        }
-        String currentQuestionToThePassage = type.getQuestionToThePassage();
-        String newQuestionToThePassage = typeRequest.getQuestionToThePassage();
-
-        if (!currentQuestionToThePassage.equals(newQuestionToThePassage)){
-            type.setQuestionToThePassage(newQuestionToThePassage);
-        }
-        String currentPassage = type.getPassage();
-        String newPassage = typeRequest.getPassage();
-
-        if (!currentPassage.equals(newPassage)){
-            type.setPassage(newPassage);
-        }
-        String currentHighlightCorrectAnswer = type.getHighlightCorrectAnswer();
-        String newHighlightCorrectAnswer = typeRequest.getHighlightCorrectAnswer();
-
-        if (!currentHighlightCorrectAnswer.equals(newHighlightCorrectAnswer)){
-            type.setHighlightCorrectAnswer(newHighlightCorrectAnswer);
-        }
-        return typeEditMapper.update(type,typeRequest);
+        return type;
     }
     private Type findById(Long id){
         return typeRepository.findById(id).orElseThrow(()-> new NotFoundException(
