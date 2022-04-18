7c9b88a9ca1a8f46e8e7ebbbcd227f91630e18da
AkylbekNurbekov <97018017+AkylbekNurbekov@users.noreply.github.com>
2022-04-18 15:53:05 +0600

    add

---
+++ src/test/java/kg/peaksoft/bilingualb4/services/impl/UserServiceImplTest.java
@@ -0,0 +1,28 @@
+package kg.peaksoft.bilingualb4.services.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void registration() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void update() {
+    }
+}
