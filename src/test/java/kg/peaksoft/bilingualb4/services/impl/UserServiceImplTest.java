e5f3c3bd8175170b7e50d4c5b46ffeda553716f9
AkylbekNurbekov <97018017+AkylbekNurbekov@users.noreply.github.com>
2022-04-20 12:40:20 +0600

    add testing methods

--- src/test/java/kg/peaksoft/bilingualb4/services/impl/UserServiceImplTest.java
+++ src/test/java/kg/peaksoft/bilingualb4/services/impl/UserServiceImplTest.java
@@ -4,9 +4,9 @@ import kg.peaksoft.bilingualb4.api.payload.UserRequest;
 import kg.peaksoft.bilingualb4.api.payload.UserResponse;
 import kg.peaksoft.bilingualb4.model.entity.User;
 import kg.peaksoft.bilingualb4.services.UserService;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
@@ -15,12 +15,11 @@ import java.util.Objects;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserServiceImplTest {
 
-
+    @Autowired
     private UserService userService;
-
-    private UserResponse userResponse;
 
     UserRequest userRequest = new UserRequest();
 
@@ -32,67 +31,67 @@ class UserServiceImplTest {
     }
 
     @Test
-    void findAll() {
-        try {
-            newUser();
-            List<User> userList = userService.findAll();
-            if (userList.size() < 1) {
-                Assertions.fail("There are no clients in the database");
-            }
-
-        } catch (Exception e) {
-            Assertions.fail("An error occurred while getting clients: " + e.getMessage());
-        }
-    }
-
-    @Test
+    @Order(1)
     void registration() {
         try {
             newUser();
-            if (Objects.equals(userRequest.getUserName(), userResponse.getUserName())
-                    || Objects.equals(userRequest.getEmail(), userResponse.getEmail())) {
-                Assertions.fail("There are several customers in the database with the same emails!\n");
-
-            }
         } catch (Exception e) {
             Assertions.fail("An error occurred while registering clients: " + e.getMessage());
         }
     }
 
     @Test
+    @Order(2)
     void findById() {
         try {
-            newUser();
-            User user = userService.findById(1L).get();
+            User user = userService.findById(2L).get();
             assertThat(user).isNotNull();
-            assertThat(user.getUserName()).isEqualTo("akylbeknurbekov01@gmail.com");
+            assertThat(user.getUserName()).isEqualTo("Akylbek");
+            assertThat(user.getEmail()).isEqualTo("akylbeknurbekov01@gmail.com");
         } catch (Exception e) {
             Assertions.fail("An error occurred while getting a client by id: " + e.getMessage());
         }
     }
 
     @Test
-    void deleteById() {
-        try {
-            newUser();
-            userService.deleteById(1L);
-        } catch (Exception e) {
-            Assertions.fail("An error occurred while deleting clients by id: " + e.getMessage());
-        }
-    }
-
-    @Test
+    @Order(3)
     void update() {
         try {
             UserRequest userRequest = new UserRequest();
             userRequest.setUserName("Talgarbek");
             userRequest.setEmail("talgarbek00@gmail.com");
             userRequest.setPassword("talgarbek00");
-
             userService.update(2L, userRequest);
 
         } catch (Exception e) {
             Assertions.fail("An error occurred while updating the client by id: " + e.getMessage());
         }
     }
+
+    @Test
+    @Order(4)
+    void findAll() {
+        try {
+            List<User> userList = userService.findAll();
+            if (userList.size() < 1) {
+                Assertions.fail("There are no clients in the database");
+            }
+        } catch (Exception e) {
+            Assertions.fail("An error occurred while getting clients: " + e.getMessage());
+        }
+    }
+
+    @Test
+    @Order(5)
+    void deleteById() {
+        try {
+            userService.deleteById(2L);
+        } catch (Exception e) {
+            Assertions.fail("An error occurred while deleting clients by id: " + e.getMessage());
+        }
+    }
+
+
+
+
 }
