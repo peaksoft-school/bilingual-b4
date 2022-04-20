96d919cf7419e3c83b0865fd6703c1a7c754f999
Muktarbek <muktarbeks0101@gmail.com>
2022-04-20 12:48:40 +0600

    test deploy final

--- src/main/java/kg/peaksoft/bilingualb4/BilingualB4Application.java
+++ src/main/java/kg/peaksoft/bilingualb4/BilingualB4Application.java
@@ -3,11 +3,18 @@ package kg.peaksoft.bilingualb4;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @AllArgsConstructor
 @SpringBootApplication
+@RestController
 public class BilingualB4Application {
+    @GetMapping
+    public String checkHealth(){
+        return "ok";
+    }
     public static void main(String[] args) {
         SpringApplication.run(BilingualB4Application.class, args);
         System.out.println("Welcome colleagues, project name is Bilingual!");
