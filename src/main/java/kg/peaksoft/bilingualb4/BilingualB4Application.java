a2bea08959f0a491dadfb8fcb45138d91f2b51d2
Muktarbek <muktarbeks0101@gmail.com>
2022-04-24 12:05:34 +0600

    test deploy final

--- src/main/java/kg/peaksoft/bilingualb4/BilingualB4Application.java
+++ src/main/java/kg/peaksoft/bilingualb4/BilingualB4Application.java
@@ -1,29 +1,23 @@
 package kg.peaksoft.bilingualb4;
 
-import kg.peaksoft.bilingualb4.model.entity.AuthInfo;
-import kg.peaksoft.bilingualb4.model.entity.Role;
-import kg.peaksoft.bilingualb4.model.entity.User;
 import kg.peaksoft.bilingualb4.repository.RoleRepository;
 import kg.peaksoft.bilingualb4.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import javax.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-@AllArgsConstructor
-@SpringBootApplication
 @RestController
+@SpringBootApplication
+@AllArgsConstructor
 public class BilingualB4Application {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
     @GetMapping("/")
-    public String checkHealth(){
+    public String greetingPage(){
         return "<h1>Welcome to Bilingual4 application!!!<h1/>";
     }
     public static void main(String[] args) {
