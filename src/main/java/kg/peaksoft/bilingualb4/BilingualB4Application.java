0b5a1298a44af5d586b9e75b355f4a6e5fa4eccb
Muktarbek <muktarbeks0101@gmail.com>
2022-04-24 15:02:54 +0600

    test deploy final

--- src/main/java/kg/peaksoft/bilingualb4/BilingualB4Application.java
+++ src/main/java/kg/peaksoft/bilingualb4/BilingualB4Application.java
@@ -1,5 +1,8 @@
 package kg.peaksoft.bilingualb4;
 
+import kg.peaksoft.bilingualb4.model.entity.AuthInfo;
+import kg.peaksoft.bilingualb4.model.entity.Role;
+import kg.peaksoft.bilingualb4.model.entity.User;
 import kg.peaksoft.bilingualb4.repository.RoleRepository;
 import kg.peaksoft.bilingualb4.repository.UserRepository;
 import lombok.AllArgsConstructor;
@@ -8,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
 
 @RestController
 @SpringBootApplication
@@ -18,36 +23,36 @@ public class BilingualB4Application {
     private final PasswordEncoder passwordEncoder;
     @GetMapping("/")
     public String greetingPage(){
-        return "<h1>Welcome to Bilingual4 application!!!<h1/>";
+        return "<h1>Welcome to Bilingual application!!!<h1/>";
     }
     public static void main(String[] args) {
         SpringApplication.run(BilingualB4Application.class, args);
         System.out.println("Welcome colleagues, project name is Bilingual!");
     }
 
-//    @PostConstruct
-//    public void init() {
-//        User user = new User();
-//        user.setUserName("Muhammed");
-//        user.setEmail("muhammed@gmail.com");
-//        user.setPassword(passwordEncoder.encode("2003"));
-//
-//        Role role = new Role();
-//        Role role1 = new Role();
-//        role.setName("ADMIN");
-//
-//        AuthInfo authInfo = new AuthInfo();
-//        authInfo.setEmail(user.getEmail());
-//        authInfo.setPassword(user.getPassword());
-//
-//        authInfo.setAuthInfo1(role);
-//
-//        user.setAuthInfo(authInfo);
-//        userRepository.save(user);
-//        role1.setName("CLIENT");
-//        roleRepository.save(role1);
-//        authInfo.setAuthInfo1(role1);
-//
-//    }
+    @PostConstruct
+    public void init() {
+        User user = new User();
+        user.setUserName("Muhammed");
+        user.setEmail("muhammed@gmail.com");
+        user.setPassword(passwordEncoder.encode("2003"));
+
+        Role role = new Role();
+        Role role1 = new Role();
+        role.setName("ADMIN");
+
+        AuthInfo authInfo = new AuthInfo();
+        authInfo.setEmail(user.getEmail());
+        authInfo.setPassword(user.getPassword());
+
+        authInfo.setAuthInfo1(role);
+
+        user.setAuthInfo(authInfo);
+        userRepository.save(user);
+        role1.setName("CLIENT");
+        roleRepository.save(role1);
+        authInfo.setAuthInfo1(role1);
+
+    }
 
 }
