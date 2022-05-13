bb25b4d8f576c5e2f53ce9533cee63e68ebe80da
admin <muhammedbatyrov1015@gmail.com>
2022-05-13 21:00:51 +0600

    results page update is done

--- src/main/java/kg/peaksoft/bilingualb4/api/payload/AuthInfoResponse.java
+++ src/main/java/kg/peaksoft/bilingualb4/api/payload/AuthInfoResponse.java
@@ -1,8 +1,13 @@
 package kg.peaksoft.bilingualb4.api.payload;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import kg.peaksoft.bilingualb4.model.entity.Role;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -12,5 +17,11 @@ public class AuthInfoResponse {
     private String email;
 
     private String token;
+
+    @JsonProperty("user_name")
+    private String userName;
+
+
+    private String role;
 
 }
---
+++ src/main/java/kg/peaksoft/bilingualb4/exception/exceptionResponse/ExceptionResponse.java
@@ -0,0 +1,13 @@
+package kg.peaksoft.bilingualb4.exception.exceptionResponse;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+public class ExceptionResponse {
+    private HttpStatus status;
+    private String message;
+
+}
