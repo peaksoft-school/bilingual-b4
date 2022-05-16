95632f7e118e29fdba8c0f3770aae77d4bfc559f
Muktarbek <muktarbeks0101@gmail.com>
2022-05-16 13:07:36 +0600

    corssorigin

--- src/main/java/kg/peaksoft/bilingualb4/api/controller/UploadFileController.java
+++ src/main/java/kg/peaksoft/bilingualb4/api/controller/UploadFileController.java
@@ -14,6 +14,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("api/file")
 @AllArgsConstructor
+@CrossOrigin(origins = "*",maxAge = 3600)
 public class UploadFileController {
 
     private AwsService awsService;
